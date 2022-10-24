1727ed8e93475e86daf07f4d7eb3d7a95291902f
Guilherme Orlandin <guiorlandin@gmail.com>
2022-10-24 02:24:49 -0300

    secont-commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const diasEl = document.querySelector("#dias")
+const horasEl = document.querySelector("#horas")
+const minutosEl = document.querySelector("#minutos")
+const segundosEl = document.querySelector("#segundos")
+
+const dataFinal = "1 Jan 2023"
+
+function countdown() {
+    const dataEsperada = new Date(dataFinal);
+    const presenteData = new Date();
+
+    const totalSegundos = (dataEsperada - presenteData) / 1000;
+
+    const dias = Math.floor(totalSegundos / 3600 / 24 );
+    const horas = Math.floor(totalSegundos/ 3600) % 24;
+    const mins = Math.floor(totalSegundos/ 60) % 60;
+    const segundos = Math.floor(totalSegundos) % 60;
+
+    
+    diasEl.innerHTML = `&nbsp${formatTime(dias)} :`;
+    horasEl.innerHTML = `&nbsp${formatTime(horas)} :`;
+    minutosEl.innerHTML = `&nbsp${formatTime(mins)} :`;
+    segundosEl.innerHTML = `${formatTime(segundos)}&nbsp`;
+
+
+    function formatTime(time){
+        return time < 10 ? `0${time}` : time;
+    }
+}
+
+countdown();
+
+setInterval(countdown, 1000);
